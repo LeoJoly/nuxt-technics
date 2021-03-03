6bc2bd55b8d93e3af53642ee5d0a5fd40338eb2c
LeoJoly <joly.leo@gmail.com>
2021-03-03 21:40:59 +0100

    weather page + weather api call

---
+++ pages/weather.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="weather">
+    <div>
+      <h1 class="weather__title">
+        The weather !
+      </h1>
+      <ul v-for="forecastDay in forecastDays" :key="forecastDay.date" class="weather__forecast">
+        <li>
+          {{ forecastDay.date }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData () {
+    const forecastDays = await axios.get(
+      'https://api.weatherapi.com/v1/forecast.json?key=2d556116f65c4f1da10145144210103&q=paris&days=7&aqi=no&alerts=no'
+    ).then(res => res.data.forecast.forecastday)
+
+    return { forecastDays }
+  }
+  // data () {
+  //   return {
+  //     forecastDays: []
+  //   }
+  // }
+  // methods: {
+  //   updateWeather (newForecast) {
+  //     this.forecastDays = newForecast
+  //   }
+  // },
+}
+</script>
